ba161c3476cc64e06d8cebab3b6e0ceefb2a4f6f
unknown <chiranwettewa>
2026-02-19 15:35:47 +0530

    first commit

---
+++ src/main/java/org/example/dto/Course.java
@@ -0,0 +1,23 @@
+package org.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course {
+
+    private int id;
+    private String name;
+    private double price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
