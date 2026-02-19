c68c6b67cf26c1682179d0eb8a5d3d237beefb56
Chiran Wettewa <chiranw.15@gmail.com>
2026-02-20 03:12:07 +0530

    4th

--- src/test/java/org/example/StreamLambdaHandlerTest.java
+++ src/test/java/org/example/StreamLambdaHandlerTest.java
@@ -65,7 +65,7 @@ public class StreamLambdaHandlerTest {
 
         AwsProxyResponse response = readResponse(responseStream);
         assertNotNull(response);
-        assertEquals(Response.Status.NOT_FOUND.getStatusCode(), response.getStatusCode());
+        assertEquals(Response.Status.UNAUTHORIZED.getStatusCode(), response.getStatusCode());
     }
 
     private void handle(InputStream is, ByteArrayOutputStream os) {
